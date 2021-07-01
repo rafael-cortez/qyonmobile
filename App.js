033dd3920ed4e4982a793536ec1edf2303ce4f45
Rafael Bradaschia Cortez <dev.rafaelbradax@gmail.com>
2021-06-30 22:57:41 -0300

    add splash screen

--- App.js
+++ App.js
@@ -3,8 +3,12 @@ import { NavigationContainer } from "@react-navigation/native"
 
 import { UserProvider } from "./src/Contexts/UserContext"
 import Routes from "./src/Routes/index"
+import RNBootSplash from "react-native-bootsplash"
 
 const App = () => {
+  React.useEffect(() => {
+    RNBootSplash.hide({ duration: 500 })
+  }, [])
   return (
     <UserProvider>
       <NavigationContainer>
